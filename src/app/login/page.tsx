0a21da8d5802fce570bfacbc55292e5b69af9654
mustafatawab <engrmustafatawab05@gmail.com>
2025-09-03 00:56:54 +0500

    comleted login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -19,10 +19,8 @@ import toast from "react-hot-toast";
 const page = () => {
   const [showPass, setShowPass] = useState<boolean>(false);
   const [form, setForm] = useState({
-    name: "",
     email: "",
     password: "",
-    confirm_password: "",
   });
 
   const handleChange = (e: any) => {
@@ -41,34 +39,16 @@ const page = () => {
   }
   const onSubmit = async (e : any) => {
     e.preventDefault()
-    if(form.password != form.confirm_password){
-        toast.error("Password does not match")
-    }
+    
   };
 
   return (
-    <main className="px-5 w-full min-h-screen flex items-center justify-center">
+    <main className="px-5 w-full min-h-screen flex flex-col items-center justify-center ">
       <Card className="w-full md:w-1/2 lg:w-1/3 space-y-4 bg-white p-5 border-0">
-        <h2 className="text-center text-3xl font-bold mb-5">Register</h2>
-
+        <h2 className="text-center text-3xl font-bold mb-5">Login</h2>
         <CardContent>
           <form action="" onSubmit={onSubmit} className="flex flex-col gap-5">
-            <div className="space-y-2">
-              <Label htmlFor="name" className="text-lg">
-                Full Name
-              </Label>
-              <Input
-                name="name"
-                value={form.name}
-                onChange={handleChange}
-                id="name"
-                type="text"
-                placeholder="Full Name"
-                className="focus:bg-slate-100 focus:border-none"
-                required
-              />
-            </div>
-
+            
             <div className="space-y-2">
               <Label htmlFor="email" className="text-lg">
                 Email
@@ -101,21 +81,7 @@ const page = () => {
               />
             </div>
 
-            <div className="space-y-2">
-              <Label htmlFor="confirm_password" className="text-lg">
-                Confirm Password
-              </Label>
-              <Input
-                name="confirm_password"
-                value={form.confirm_password}
-                onChange={handleChange}
-                id="confirm_password"
-                type="password"
-                placeholder="Confirm Password"
-                className="focus:bg-slate-100 focus:border-none"
-                required
-              />
-            </div>
+           
             <div className="flex items-start gap-3">
               <Checkbox id="toggle" onChange={onCheckboxChange} checked={showPass} />
               <Label htmlFor="toggle">Show Password</Label>
@@ -125,20 +91,20 @@ const page = () => {
               type="submit"
               className="bg-slate-800 hover:bg-slate-700 cursor-pointer"
             >
-              Sign Up
+              Login
             </Button>
           </form>
         </CardContent>
         <CardFooter className="flex flex-col gap-3">
           <p className="text-center">
-            Already have an account ?{" "}
-            <Link href={"/login"} className="cursor-pointer text-blue-700 ">
-              Login{" "}
+            Don't have an account ?{" "}
+            <Link href={"/register"} className="cursor-pointer text-blue-700 ">
+              Sign Up{" "}
             </Link>{" "}
           </p>
           <span>OR</span>
           <Button variant="outline" className="w-full">
-            Sign Up with Google
+            Login with Google
           </Button>
         </CardFooter>
       </Card>
