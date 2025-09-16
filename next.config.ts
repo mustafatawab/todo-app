21af965834df0f70e7b8c791f4e226813f6e8248
mustafatawab <engrmustafatawab05@gmail.com>
2025-09-16 15:45:42 +0500

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
-
+import path from "path";
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.snapshot = {
+      ...config,
+      managedPaths : [/^(.+?[\\/]node_modules[\\/])/]
+    }
+
+    return config
+  }
 };
 
 export default nextConfig;
